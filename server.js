d065d49c32bbf00535972046b391c6f96accd3be
Henry Diamond <henrydiamond94@gmail.com>
2020-07-08 01:52:26 +0400

    Added basic api request validation using express validator

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+// Required Modules for App
 const express = require('express');
 const connectDB = require('./config/db');
+
+// Express App
 const app = express();
+
+// Connect  Db
+connectDB();
+
+// init Mildware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
 
-// Connect  Db
-connectDB();
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
