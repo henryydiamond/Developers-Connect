9ba98ef0cb71ee26aa7cea4460028acf883b9a8d
Henry Diamond <henrydiamond94@gmail.com>
2020-07-07 20:25:45 +0400

    Connected the server to mongoose atlas

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.get('/', (req, res) => {
+  res.send('API Running');
 });
+
+// Connect  Db
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
