328fd49fc30b1f5f33e1e02bcbdd6448b511aad2
Henry Diamond <henrydiamond94@gmail.com>
2020-07-19 18:21:48 +0400

    Added a put request that allows  users to insert their educational background

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -237,4 +237,59 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+// @route       PUT api/profile/education/:edu_id
+// @desc        Add profile education
+// @access      Private
+
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+      check('from', 'From is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //// Create instance for fields in the database
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    // Object that holds users input
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
